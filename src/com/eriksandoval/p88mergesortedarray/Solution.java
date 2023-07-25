5ae57645cfd47c44e5ab835b2960961e4ec8b8b6
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-24 19:46:39 -0500

    Changes to README and package structure.

---
+++ src/com/eriksandoval/p88mergesortedarray/Solution.java
@@ -0,0 +1,20 @@
+package com.eriksandoval.p88mergesortedarray;
+
+public class Solution {
+
+    /*
+     * This void method will merge two sorted (non-decreasing order) arrays into one sorted array.
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // We will first need to create a new array that will hold the sorted values of nums1 and nums2.
+        int[] nums3 = new int[m + n]; // Nums3 will be the size of nums1 and nums2 combined.
+
+        // We will also need to keep track of where pointers are in nums1 and nums2 (indices).
+        int i = 0; // Default value of index 0 for nums1.
+        int j = 0; // Default value of index 0 for nums2. 
+        
+
+    }
+    
+}
